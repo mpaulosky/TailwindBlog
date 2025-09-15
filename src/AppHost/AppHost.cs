539e17aa6d5a3d8bfaf75c88d5c15dd5de81eac2
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-09-15 11:02:24 -0700

    Checkpoint from VS Code for coding agent session

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,13 +2,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add SQL Server resource
 var sqlserver = builder.AddSqlServer("sqlserver");
-var database = sqlserver.AddDatabase("tailwinddb");
-
-builder.AddProject<Projects.Web>("web")
-	.WithReference(database);
+var database = sqlserver.AddDatabase("securitydb");
 
 // TODO: Add MongoDB
 
-// TODO: Add Website
+
+builder.AddProject<Projects.Web>("web")
+	.WithReference(database)
+	.WithReference(mongoDatabase);
 
 builder.Build().Run();
